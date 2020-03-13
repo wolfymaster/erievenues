6045909fc298aa12e0beab67c2747791bcbd55dc
Dakota Walker <dakota.a.walker@icloud.com>
2020-03-13 15:29:12 -0400

    Add migrations

--- database/migrations/2020_03_13_192204_create_venue_types_table.php
+++ database/migrations/2020_03_13_192204_create_venue_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVenuesTable extends Migration
+class CreateVenueTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateVenuesTable extends Migration
      */
     public function up()
     {
-        Schema::create('venues', function (Blueprint $table) {
+        Schema::create('venue_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->text('description')->nullable();
-
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateVenuesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('venues');
+        Schema::dropIfExists('venue_types');
     }
 }
--- database/migrations/2020_03_13_192711_create_payment_policies__table.php
+++ database/migrations/2020_03_13_192711_create_payment_policies__table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVenuesTable extends Migration
+class CreatePaymentPoliciesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateVenuesTable extends Migration
      */
     public function up()
     {
-        Schema::create('venues', function (Blueprint $table) {
+        Schema::create('payment_policies_', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->text('description')->nullable();
-
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateVenuesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('venues');
+        Schema::dropIfExists('payment_policies_');
     }
 }
